cf297db1880e0f5df184526b943e0470b3f424d7
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-08-01 16:21:14 +0300

    lab06 finish

---
+++ src/Component/Footer.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Footer extends Component{
+    render(){
+        return(
+            <footer className="d-flex justify-content-center align-items-center p-3 bg-dark text-light">
+                <p>All Rights Reserved &copy; 2021, ASAC Team Mohammad Alzoubi.</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
